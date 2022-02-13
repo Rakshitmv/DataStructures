1c2b331ef3913161002e44b6d675ea63d4f13098
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-13 09:26:09 +0530

    [Rakshit] Add: UC1_LinkedList program added

---
+++ DataStructures/LinkedList/LinkedListNode.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList
+{
+    public class LinkedListNode
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Linked list", node.data);
+        }
+        public void AddInReverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = temp;
+                head.next = temp;
+            }
+        }
+        public Node RemoveFirstNode(int data)
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+        public Node RemoveLastNode()
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            if (head.next == null)
+            {
+                return null;
+            }
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
+        public int Search(int data)
+        {
+            Node temp = head;
+            int count = 0;
+            while (temp != null)
+            {
+                if (temp.data == data) return count;
+                temp = temp.next;
+                count++;
+            }
+            return -1;
+        }
+        public void DeleteNodeAtParticularPosition(int position)
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+                return;
+            }
+            Node temp = this.head;
+            if (position == 0)
+            {
+                this.head = temp.next;
+                return;
+            }
+            for (int i = 0; temp != null && i < position - 1; i++)
+            {
+                temp = temp.next;
+            }
+            if ((temp.next == null) || (temp.next == null))
+            {
+                return;
+            }
+            Node next = temp.next.next;
+            temp.next = next;
+            Size();
+        }
+        public void Size()
+        {
+            Node temp = this.head;
+            int count = 0;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+                count++;
+            }
+            Console.WriteLine("Length of linkedList is : " + " " + count);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp.next != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,42 @@
+﻿using DataStructures.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListNode list = new LinkedListNode();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.AddInReverseOrder(10);
+            list.AddInReverseOrder(20);
+            list.AddInReverseOrder(30);
+            int pos = list.Search(20);
+            int requiredPos = list.Search(10);
+            list.RemoveFirstNode(1);
+            list.RemoveLastNode();
+            list.Search(20);
+            if (pos == -1)
+            {
+                Console.WriteLine("Element is not present in the list");
+            }
+            else
+            {
+                Console.WriteLine(pos);
+            }
+            list.DeleteNodeAtParticularPosition(pos);
+            list.Display();
+            list.Size();
+            Console.ReadLine();
+        }
+    }
+    
+}
