a703c58464901728a5d8d6b0b20e238811277335
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-14 12:37:01 +0530

    [Rakshit] Add: Stack program added

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,9 +1,5 @@
-﻿using DataStructures.LinkedList;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using System.Collections;
 
 namespace DataStructures
 {
@@ -11,30 +7,12 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            LinkedListNode list = new LinkedListNode();
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
-            list.Display();
-            list.AddInReverseOrder(10);
-            list.AddInReverseOrder(20);
-            list.AddInReverseOrder(30);
-            int pos = list.Search(20);
-            int requiredPos = list.Search(10);
-            list.RemoveFirstNode(1);
-            list.RemoveLastNode();
-            list.Search(20);
-            if (pos == -1)
-            {
-                Console.WriteLine("Element is not present in the list");
-            }
-            else
-            {
-                Console.WriteLine(pos);
-            }
-            list.DeleteNodeAtParticularPosition(pos);
-            list.Display();
-            list.Size();
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Pop();
+            stack.Display();
             Console.ReadLine();
         }
     }
---
+++ DataStructures/Stack/Stack.cs
@@ -0,0 +1,64 @@
+﻿using DataStructures.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.StackQueue
+{
+    public class Stack
+    {
+        public Node head;
+        public Node Push(int data)
+        {
+            Node newnode = new Node(data);
+            if (head == null)
+            {
+                head = newnode;
+                return head;
+            }
+            else
+            {
+                Node temp = head;
+                head = newnode;
+                newnode.next = temp;
+                return head;
+            }
+
+        }
+        public Node Pop()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return head;
+            }
+            else
+            {
+                Console.WriteLine(head.data + " Removed from the stack");
+                head = head.next;
+                return head;
+            }
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(temp.data);
+                    temp = temp.next;
+                }
+            }
+            Console.WriteLine("null");
+        }
+
+    }
+}
+
