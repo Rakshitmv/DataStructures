7b5528703f852a8d42292434dbb989830428f7df
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-14 12:48:52 +0530

    [Rakshit] Add: Queue program added

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructures.Queue;
+using System;
 using System.Collections;
 
 namespace DataStructures
@@ -7,13 +8,13 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Stack stack = new Stack();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            stack.Pop();
-            stack.Display();
-            Console.ReadLine();
+            Queues queue = new Queues();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Display();
         }
     }
     
